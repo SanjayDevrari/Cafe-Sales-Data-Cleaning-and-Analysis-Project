be3d4787e578f34c71705daf608144ed36d48ecf
Sanjay Devrari <devrarisanjay18@gmail.com>
2026-04-15 00:14:17 +0530

    data_cleaning.py

--- Notebooks/data_cleaning.py
+++ Notebooks/data_cleaning.py
@@ -8,7 +8,8 @@
 
 # Import the pandas library for data manipulation
 import pandas as pd
-
+# Import numpy (needed to use NaN values)
+import numpy as np
 # Define the dataset path (CSV file hosted on GitHub)
 path = "https://raw.githubusercontent.com/SanjayDevrari/Cafe-Sales-Data-Cleaning-and-Analysis-Project/refs/heads/main/Data/dirty_cafe_sales.csv"
 
@@ -35,9 +36,32 @@ print(df.head())
 print("\nDataset Information:")
 print(df.info())
 
-
 # ---------------------------------------------------------
 # 4️⃣ Statistical summary of numerical columns
 # ---------------------------------------------------------
 print("\nStatistical Summary:")
 print(df.describe())
+
+# ---------------------------------------------------------
+# 1️⃣ Check Missing Values
+# ---------------------------------------------------------
+print("Missing Values in Each Column:")
+print(df.isnull().sum())
+
+# ---------------------------------------------------------
+# 2️⃣ Replace Invalid Entries with NaN
+# ---------------------------------------------------------
+df.replace(["ERROR", "UNKNOWN"], np.nan, inplace=True)
+
+
+# ---------------------------------------------------------
+# 3️⃣ Check Duplicate Records
+# ---------------------------------------------------------
+print("\nNumber of Duplicate Rows:")
+print(df.duplicated().sum())
+
+
+# ---------------------------------------------------------
+# 4️⃣ Remove Duplicate Records
+# ---------------------------------------------------------
+df.drop_duplicates(inplace=True)
